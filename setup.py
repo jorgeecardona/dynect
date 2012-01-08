18455dd2f1e545a5450311ecb243a5a697c96d7a
Jorge E. Cardona <jorgeecardona@gmail.com>
2012-01-08 12:51:09 -0500

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = lambda *x: join(abspath(dirname(__file__)), *x)
 
 setup(
     name='dynect',
-    version='0.0.6c',
+    version='0.0.7',
     description='Wrapper library to Dynect API.',
     long_description=here('README.rst'),
     packages=['dynect'],
